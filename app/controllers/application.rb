44321b387a95dc15fb67480749516d33a4b913a7
Britt Ballard <britt@thoughtbot.com>
2015-07-14 23:25:46 -0500

    Move config logic to the correct application.rb

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
   helper :all
   protect_from_forgery :secret => '8f8564e819692662964e08450dfd1172'
-  config.assets.precompile += %w(soundmanager2.swf soundmanager2_flash9.swf)
 end
